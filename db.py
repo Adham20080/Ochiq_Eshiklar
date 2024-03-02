4b5f1cf77469627b5be1f3ef7ca020f21184add2
ahmadjon2008 <robotoprotecno2008@gmail.com>
2024-03-02 11:39:07 +0500

    Commit

--- db.py
+++ db.py
@@ -3,11 +3,13 @@ import sqlite3
 conn = sqlite3.connect('db.db')
 
 conn.execute("""
-CREATE TABLE IF NOT EXISTS users (
-id INTEGER PRIMARY KEY
-first_name TEXT NOT NULL
-last_name TEXT NULL
-username TEXT NOT NULL)""")
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY
+        first_name TEXT NOT NULL
+        last_name TEXT NULL
+        username TEXT NOT NULL
+    )"""
+             )
 
 
 class Database:
